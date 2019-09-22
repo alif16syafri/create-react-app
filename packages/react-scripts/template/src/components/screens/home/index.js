4540f174bc20fb3a6a0a946f85f97c5363350e50
alif16syafri <alif16syafri@hotmail.com>
2019-09-22 20:32:49 +0700

    feat: add basic config directories

---
+++ packages/react-scripts/template/src/components/screens/home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HomeScreen extends Component {
+  render() {
+    return (
+      <div>HOME SCREEN</div>
+    );
+  }
+}
+
+export default HomeScreen;
